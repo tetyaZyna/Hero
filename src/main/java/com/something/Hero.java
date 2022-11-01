5188eda235102ea16a8c977a9eec6e93d916fbbf
Zina <sv2408sv@gmail.com>
2022-11-01 21:02:24 +0100

    Add first classes

---
+++ src/main/java/com/something/Hero.java
@@ -0,0 +1,13 @@
+package com.something;
+
+public class Hero {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+    public void attackEnemy(){
+        System.out.println("Герой " + name + " атакував");
+    }
+
+}
---
+++ src/main/java/com/something/TrainingGround.java
@@ -0,0 +1,8 @@
+package com.something;
+
+public class TrainingGround {
+    public static void main(String[] args) {
+        Hero men = new Hero();
+        men.attackEnemy();
+    }
+}
