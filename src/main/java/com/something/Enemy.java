18b83b1ace513ba949e57aac121e54321a5fcced
tetyaZyna <73794266+tetyaZyna@users.noreply.github.com>
2022-11-07 21:18:34 +0100

    Do task 6

--- src/main/java/com/something/Enemy.java
+++ src/main/java/com/something/Enemy.java
@@ -13,7 +13,7 @@ public class Enemy implements Mortal{
 
     public void setHealth(int health) {
         Enemy.health = health;
-    }//IDE предлагает использовать Enemy вместо this (зачем использовать сеттер)
+    }
 
     public static int takeDamage(int damage){
         health = health - damage;
